90978f2be984a9ad9f258f9292512f1c6d2ce942
Sergio Gamez <sergiovdg2016@gmail.com>
2025-01-29 08:30:23 +0100

    Arreglo script de desplegado y borrado de Stack AWS

--- delete_stack.bat
+++ delete_stack.bat
@@ -1,6 +1,4 @@
 set REGION=us-east-1
 set STACK_NAME=Pila-Ubuntu22-04
 
-aws cloudformation delete-stack ^
-    --stack-name "%STACK_NAME%" ^
-    --region "%REGION%"
+aws cloudformation delete-stack --stack-name "%STACK_NAME%" --region "%REGION%"
--- deploy_stack.bat
+++ deploy_stack.bat
@@ -1,7 +1,6 @@
 set REGION=us-east-1
 set STACK_NAME=Pila-Ubuntu22-04
 set TEMPLATE_FILE=parameters.yaml
-set TAGS=Name=Stack-AWS-CLI
 
 aws cloudformation deploy ^
     --template-file "%TEMPLATE_FILE%" ^
